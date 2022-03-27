6ec63c6eb65ae23f5d24b4a92163bcfa959706f4
LisbethIraheta <lisbethiraheta@outlook.com>
2022-03-26 21:24:16 -0600

    barra de navegación creada

--- routes/home.py
+++ routes/home.py
@@ -1,7 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 home = Blueprint("home", __name__)
 
 @home.route("/")
 def main():
-    return "Funciona la dirección web"
+    return render_template("japan.html")
